eb77118ab655a72f41c55e5a7412a8a569e9f0a3
franchu7 <73340817+franchu7@users.noreply.github.com>
2023-12-24 00:21:43 +0100

    Handle exceptions

--- openelevationservice/server/grpc/grpc_server.py
+++ openelevationservice/server/grpc/grpc_server.py
@@ -1,4 +1,5 @@
 from concurrent import futures
+from sqlalchemy.exc import SQLAlchemyError
 from openelevationservice.server.api import querybuilder, views
 from openelevationservice.server.api.api_exceptions import InvalidUsage
 from openelevationservice.server.utils import convert
@@ -8,43 +9,52 @@ from . import openelevation_pb2 as defs
 from . import openelevation_pb2_grpc
 from shapely import wkt
 
+def handle_exceptions(func):
+    def wrapper(self, request, context):
+        try:
+            return func(self, request, context)
+        except InvalidUsage as error:
+            context.abort(grpc.StatusCode.INTERNAL, error.to_dict().get('message'))
+        except SQLAlchemyError as error:
+            context.abort(grpc.StatusCode.INTERNAL, 'Could not connect to database.')
+        except Exception as error:
+            print(error)
+            context.abort(grpc.StatusCode.INTERNAL, 'An unexpected error occurred.')
+    return wrapper
+
 class OpenElevationServicer(openelevation_pb2_grpc.OpenElevationServicer):
     """Provides methods that implement functionality of route guide server."""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    @handle_exceptions
     def PointElevation(self, request, context):
-        try:
-            geom = convert.point_to_geometry([request.lon, request.lat])
-            geom_queried = querybuilder.point_elevation(geom, 'point', 'srtm')
-            geom_shaped = wkt.loads(geom_queried)
-            point_3d = list(geom_shaped.coords[0])
-            elevation = int(point_3d[2])
-            return defs.Elevation(value=elevation)
-        except InvalidUsage as error:
-            context.abort(grpc.StatusCode.INTERNAL, error.to_dict().get('message'))
+        geom = convert.point_to_geometry([request.lon, request.lat])
+        geom_queried = querybuilder.point_elevation(geom, 'point', 'srtm')
+        geom_shaped = wkt.loads(geom_queried)
+        point_3d = list(geom_shaped.coords[0])
+        elevation = int(point_3d[2])
+        return defs.Elevation(value=elevation)
 
+    @handle_exceptions
     def LineElevation(self, request, context):
-        try: 
-            geom = convert.polyline_to_geometry([
-                [request.start.lon, request.start.lat],
-                [request.end.lon, request.end.lat]
-            ])
-            geom_queried = querybuilder.line_elevation(geom, 'polyline', 'srtm')
-            geom_shaped = wkt.loads(views.zero_len_line_format(geom_queried))
-            
-            result = []
-            for point in list(geom_shaped.coords):
-                result.append(defs.LatLonElevation(
-                    lon=point[0],
-                    lat=point[1],
-                    elevation=int(point[2])
-                ))
+        geom = convert.polyline_to_geometry([
+            [request.start.lon, request.start.lat],
+            [request.end.lon, request.end.lat]
+        ])
+        geom_queried = querybuilder.line_elevation(geom, 'polyline', 'srtm')
+        geom_shaped = wkt.loads(views.zero_len_line_format(geom_queried))
+        
+        result = []
+        for point in list(geom_shaped.coords):
+            result.append(defs.LatLonElevation(
+                lon=point[0],
+                lat=point[1],
+                elevation=int(point[2])
+            ))
 
-            return defs.LineResponse(points=result)
-        except InvalidUsage as error:
-            context.abort(grpc.StatusCode.INTERNAL, error.to_dict().get('message'))
+        return defs.LineResponse(points=result)
         
     def _format_area_request(self, request):
         min_lat = request.bottomLeft.lat
@@ -59,23 +69,21 @@ class OpenElevationServicer(openelevation_pb2_grpc.OpenElevationServicer):
             [min_lon, min_lat]
         ]
 
+    @handle_exceptions
     def AreaPointsElevation(self, request, context):
-        try:
-            geom = convert.polygon_to_geometry(self._format_area_request(request))
-            geom_queried = querybuilder.polygon_elevation(geom, 'polygon', 'srtm')
-            geom_shaped = wkt.loads(geom_queried)
-            
-            result = []
-            for point in list(geom_shaped.coords):
-                result.append(defs.LatLonElevation(
-                    lon=point[0],
-                    lat=point[1],
-                    elevation=int(point[2])
-                ))
+        geom = convert.polygon_to_geometry(self._format_area_request(request))
+        geom_queried = querybuilder.polygon_elevation(geom, 'polygon', 'srtm')
+        geom_shaped = wkt.loads(geom_queried)
+        
+        result = []
+        for point in list(geom_shaped.coords):
+            result.append(defs.LatLonElevation(
+                lon=point[0],
+                lat=point[1],
+                elevation=int(point[2])
+            ))
 
-            return defs.AreaPointsResponse(points=result)
-        except InvalidUsage as error:
-            context.abort(grpc.StatusCode.INTERNAL, error.to_dict().get('message'))
+        return defs.AreaPointsResponse(points=result)
     
     def _create_proto_geo_polygon(self, coordinates):
         return defs.Area(boundaries=[
@@ -87,35 +95,32 @@ class OpenElevationServicer(openelevation_pb2_grpc.OpenElevationServicer):
             ]) for bondary in coordinates            
         ])
 
+    @handle_exceptions
     def AreaRangesElevation(self, request, context):
-        try:
-            geom = convert.polygon_to_geometry(self._format_area_request(request))
-            collection_queried, range_queried, avg_queried = querybuilder.polygon_coloring_elevation(geom, 'srtm')
-            
-            result = []
-            for feature in collection_queried['features']:
-                heightBase = int(feature['properties']['heightBase'])
-                if feature['geometry']['type'] == 'Polygon':
+        geom = convert.polygon_to_geometry(self._format_area_request(request))
+        collection_queried, range_queried, avg_queried = querybuilder.polygon_coloring_elevation(geom, 'srtm')
+        
+        result = []
+        for feature in collection_queried['features']:
+            heightBase = int(feature['properties']['heightBase'])
+            if feature['geometry']['type'] == 'Polygon':
+                result.append(defs.UnitedArea(
+                    baseElevation=heightBase,
+                    area=self._create_proto_geo_polygon(feature['geometry']['coordinates']),
+                ))
+            else:
+                for polygon in feature['geometry']['coordinates']:
                     result.append(defs.UnitedArea(
                         baseElevation=heightBase,
-                        area=self._create_proto_geo_polygon(feature['geometry']['coordinates']),
+                        area=self._create_proto_geo_polygon(polygon),
                     ))
-                else:
-                    for polygon in feature['geometry']['coordinates']:
-                        result.append(defs.UnitedArea(
-                            baseElevation=heightBase,
-                            area=self._create_proto_geo_polygon(polygon),
-                        ))
-            
-            return defs.AreaRangesResponse(
-                unions=result,
-                minElevation=range_queried[0],
-                maxElevation=range_queried[1],
-                avgElevation=avg_queried,
-            )
-        except InvalidUsage as error:
-            context.abort(grpc.StatusCode.INTERNAL, error.to_dict().get('message'))
-
+        
+        return defs.AreaRangesResponse(
+            unions=result,
+            minElevation=range_queried[0],
+            maxElevation=range_queried[1],
+            avgElevation=avg_queried,
+        )
 
 def grpc_serve(port_url):
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
