fc9169c5568a55afcde6ec75bb692afa3e2cb214
Alvaro Olmedo <olmedokenpo@gmail.com>
2025-02-18 10:06:47 +0100

    documentation functions in elevation_query_parallel

--- openelevationservice/server/api/elevation_query_parallel.py
+++ openelevationservice/server/api/elevation_query_parallel.py
@@ -49,16 +49,35 @@ FROM polygons;
 
 
 def classify_elevation(features_collection, min_height, max_height, num_ranges=23, no_data_value=-9999):
-    
+    """
+    Categorizes elevation values into discrete ranges and assigns color bands.
+
+    :param features_collection: GeoJSON feature collection containing elevation data.
+    :type features_collection: dict
+
+    :param min_height: Minimum elevation value in the dataset.
+    :type min_height: float
+
+    :param max_height: Maximum elevation value in the dataset.
+    :type max_height: float
+
+    :param num_ranges: Number of elevation ranges (default: 23).
+    :type num_ranges: int
+
+    :param no_data_value: Value assigned to pixels with missing data (default: -9999).
+    :type no_data_value: int
+
+    :returns: A feature collection with categorized elevation values.
+    :rtype: dict
+    """
+
     range_div = (max_height - min_height + 1) / num_ranges
 
-    
     classified_features = {
         "type": "FeatureCollection",
         "features": []
     }
 
-    
     for feature in features_collection['features']:
         
         height = feature['properties']['heightBase']
@@ -70,14 +89,12 @@ def classify_elevation(features_collection, min_height, max_height, num_ranges=2
                 num_ranges
             ), -1
         )
-        
-       
+          
         if color_range == -1:
             height_base = no_data_value 
         else:
             height_base = math.ceil(color_range * range_div + min_height)
-
-       
+ 
         classified_feature = {
             "type": "Feature",
             "geometry": feature['geometry'],
@@ -86,14 +103,22 @@ def classify_elevation(features_collection, min_height, max_height, num_ranges=2
                 "colorRange": color_range
             }
         }
-
-        
+   
         classified_features["features"].append(classified_feature)
 
     return classified_features
 
 
 def process_union(input_data):
+    """
+    Merges polygons that share the same elevation value into a unified geometry.
+
+    :param input_data: Tuple containing an elevation value and a list of polygon geometries.
+    :type input_data: tuple(float, list[Polygon])
+
+    :returns: A list of GeoJSON features with merged polygons.
+    :rtype: list[dict]
+    """
     height, polygons = input_data
     union_polygons = unary_union(polygons)
 
@@ -114,6 +139,21 @@ def process_union(input_data):
 
 
 def group_tiles_by_height_parallel(data, num_processes=12, chunk_size=5):
+    """
+    Groups tiles by elevation value and merges them in parallel for improved performance.
+
+    :param data: Feature collection containing polygons with elevation values.
+    :type data: dict
+
+    :param num_processes: Number of parallel processes to use (default: 12).
+    :type num_processes: int
+
+    :param chunk_size: Number of elements per processing chunk (default: 5).
+    :type chunk_size: int
+
+    :returns: A feature collection with merged polygons grouped by elevation.
+    :rtype: dict
+    """
     
     groupings = {}
     
