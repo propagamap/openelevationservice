9347cc6cecf870559cd74f30c5fce9284680bb72
franchu7 <francisco.calles@uah.es>
2025-02-25 17:00:49 +0100

    Set multiprocessing start method to "spawn" in run_grpc_server.py

--- run_grpc_server.py
+++ run_grpc_server.py
@@ -1,10 +1,11 @@
-import sys
 from openelevationservice.server.grpc.grpc_server import grpc_serve
 from openelevationservice.server.utils.logger import get_logger
+from multiprocessing import set_start_method
 
 log = get_logger(__name__)
 
 def main():
+    set_start_method("spawn")
     """Starts the gRPC server"""
     grpc_url = '0.0.0.0:5000'
     log.info("gRPC server starting on port {}".format(grpc_url))
