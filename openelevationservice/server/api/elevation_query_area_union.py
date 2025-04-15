a1de5a783c0c1c1c6e0f5a887db6a89cba403974
Alvaro Olmedo <olmedokenpo@gmail.com>
2025-04-15 10:24:25 +0200

    change SQL query to test favourable
    improves a few extra milliseconds

--- openelevationservice/server/api/elevation_query_area_union.py
+++ openelevationservice/server/api/elevation_query_area_union.py
@@ -4,63 +4,6 @@ import math
 from collections import defaultdict
 from shapely import unary_union
 
-#OLD-Original
-# PIXEL_POLYGONS_WITH_HEIGHT_QUERY = text(
-#     """
-#     WITH query_geom AS (
-#     SELECT ST_SetSRID(ST_GeomFromText(:polygon), 4326) AS geom
-# ),
-# pixels AS (
-#     SELECT 
-#         (ST_PixelAsPolygons(
-#             ST_Clip(oes_cgiar.rast, query_geom.geom, -32768),
-#             1, False
-#         )).geom AS geometry, 
-#         (ST_PixelAsPolygons(
-#             ST_Clip(oes_cgiar.rast, query_geom.geom, -32768),
-#             1, False
-#         )).val AS height  
-#     FROM query_geom 
-#     JOIN oes_cgiar 
-#     ON ST_Intersects(oes_cgiar.rast, query_geom.geom)
-# )
-# SELECT 
-#     ST_AsText(geometry) AS wkt,
-#     height
-# FROM pixels
-# WHERE height != -32768;
-
-#     """
-# )
-#End-OLD-Original
-
-#opcion1
-# PIXEL_POLYGONS_WITH_HEIGHT_QUERY = text(
-#     """
-#     WITH query_geom AS (
-#         SELECT ST_SetSRID(ST_GeomFromText(:polygon), 4326) AS geom
-#     ),
-#     pixels AS (
-#         SELECT 
-#             p.geom AS geometry, 
-#             p.val AS height  
-#         FROM query_geom 
-#         JOIN oes_cgiar ON ST_Intersects(oes_cgiar.rast, query_geom.geom),
-#         LATERAL ST_PixelAsPolygons(
-#             ST_Clip(oes_cgiar.rast, query_geom.geom, -32768),
-#             1, False
-#         ) AS p
-#     )
-#     SELECT 
-#         ST_AsText(geometry) AS wkt,
-#         height
-#     FROM pixels
-#     WHERE height != -32768;
-#     """
-# )
-#End-opcion1
-
-#opcion2
 PIXEL_POLYGONS_WITH_HEIGHT_QUERY = text(
     """
     WITH query_geom AS (
@@ -85,7 +28,6 @@ PIXEL_POLYGONS_WITH_HEIGHT_QUERY = text(
     FROM pixels;
     """
 )
-#End-opcion2
 
 def group_and_union_geometries(geometries_by_height, min_height, max_height, num_ranges):
     """
