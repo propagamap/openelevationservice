be6cd30f2c7bbf18bc9dcb8b27705ac2ff5a1cbc
Alvaro Olmedo <olmedokenpo@gmail.com>
2025-06-09 09:31:14 +0200

    update of grpc types

--- grpc/protoc_node.js
+++ grpc/protoc_node.js
@@ -1,14 +1,15 @@
 const fs = require('fs');
 const path = require('path');
-const subprocess = require('child_process');
+const childProcess  = require('child_process');
 
-if (process.argv.length !== 4) {
-  console.error('Invalid number of arguments\nUsage: node protoc_node.js <proto-src> <node-dist>\n');
-  process.exit(1);
+if (process.argv.length < 4) {
+    console.error('Usage: node protoc_node.js <proto_path> <output_path>');
+    process.exit(1);
 }
 
-const protoPath = path.join(__dirname, process.argv[2]);
-const distPath = path.join(__dirname, process.argv[3]);
+const distPath = path.join(__dirname, process.argv[2]);
+const protoPath = path.join(__dirname, process.argv[3]);
+const isWeb = process.argv.includes('--web');
 
 if (!fs.existsSync(distPath)) {
   fs.mkdirSync(distPath, { recursive: true });
@@ -16,26 +17,27 @@ if (!fs.existsSync(distPath)) {
 
 const protoDefs = fs.readdirSync(protoPath).filter((file) => file.endsWith('.proto'));
 
-if (protoDefs.length === 0) {
-  console.error(`No proto files found in "${protoPath}"\n`);
-  process.exit(2);
+let command = ''
+if (isWeb) {
+  command = `npm run __protoc -- --js_out=import_style=commonjs:${distPath}/ --grpc-web_out=import_style=commonjs+dts,mode=grpcwebtext:${distPath}/ --plugin=protoc-gen-grpc=protoc-gen-grpc-web -I ${protoPath}/ ${protoDefs.join(' ')}`;
+} else {
+  command = `npm run __protoc -- --js_out=import_style=commonjs,binary:${distPath}/ --ts_out=grpc_js:${distPath}/ --grpc_out=grpc_js:${distPath}/ -I ${protoPath}/ ${protoDefs.join(' ')}`;
 }
 
-const command = `npm run __protoc -- --js_out=import_style=commonjs,binary:${distPath} --ts_out=grpc_js:${distPath} --grpc_out=grpc_js:${distPath} -I ${protoPath} ${protoDefs.join(' ')}`;
-
-subprocess.exec(command, (err, stdout) => {
+childProcess.exec(command, (err, stdout) => {
   if (err) {
     console.log(err);
-    process.exit(3);
+    return;
   }
   console.log(stdout);
 
   const services = protoDefs.map((protoDef) => protoDef.substring(0, protoDef.lastIndexOf('.')));
 
-  const indexDTSContent = services.map((serviceName) => `export * from './${serviceName}_grpc_pb';\nexport * from './${serviceName}_pb';\n`).join('');
+  const fileSuffix = isWeb ? '_grpc_web_pb' : '_grpc_pb';
+  const indexDTSContent = services.map((serviceName) => `export * from './${serviceName}${fileSuffix}';\nexport * from './${serviceName}_pb';\n`).join('');
   fs.writeFileSync(path.join(distPath, 'index.d.ts'), indexDTSContent);
 
-  const indexJSImports = services.map((serviceName) => `  ...require('./${serviceName}_grpc_pb'),\n  ...require('./${serviceName}_pb')`).join(',\n');
+  const indexJSImports = services.map((serviceName) => `  ...require('./${serviceName}${fileSuffix}'),\n  ...require('./${serviceName}_pb')`).join(',\n');
   const indexJSContent = 'module.exports = {\n' + indexJSImports + '\n};\n';
   fs.writeFileSync(path.join(distPath, 'index.js'), indexJSContent);
 });
